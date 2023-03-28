457d536918296d3fc4dffcf1de0c38fd24e09c21
Shielden Christian Caberoy <129098845+Shielden17@users.noreply.github.com>
2023-03-28 16:29:42 +0800

    Update and rename README.md to calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+elif operation =="2":
+    print( num1 - num2)
+
+elif operation =="3":
+    print( num1 * num2)
+
+elif operation =="4":
+    print( num1 / num2)
+
+else:
+    print("invalid Entry")
